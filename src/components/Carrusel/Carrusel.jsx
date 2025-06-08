c25522f013c441d4f4ef610e7b3af59d39cbc516
Jazmín Rojas <tucorreo@gmail.com>
2025-06-08 15:04:20 -0500

    Actualización03

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -1,7 +1,7 @@
-// Carrusel.jsx
+
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import './Carrusel.css';
+import './Carrusel.css'; 
 
 const imagesByRoute = {
   '/': [
